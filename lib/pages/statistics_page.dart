f797fef49b999d408e314932faad4112899b4828
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2025-01-01 21:59:34 +0100

    graph start

--- lib/pages/statistics_page.dart
+++ lib/pages/statistics_page.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:habiquest/common.dart';
 
 class StatisticsPage extends StatefulWidget {
   const StatisticsPage({super.key});
@@ -11,9 +13,10 @@ class StatisticsPage extends StatefulWidget {
 class _StatisticsPageState extends State<StatisticsPage> {
 
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-
+  
   @override
   Widget build(BuildContext context) {
+    printMessage(firestore.collection('users').doc('habits').get());
     return const Placeholder();
   }
 }
